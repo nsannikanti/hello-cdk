f840a22cb47e31cb40777e65b3120a5b4f7f43d1
sannikantin1008@gmail.com <sannikantin1008@gmail.com>
2024-01-21 14:43:22 +0530

    added for EC2 instance creation

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,23 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
 import { aws_ec2 as ec2 } from 'aws-cdk-lib';
-import { aws_iam as iam } from 'aws-cdk-lib';
+
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'MyFirstBucketasdasdfgfds19012024', {
+    new s3.Bucket(this, 'MyFirstBucketdchgdcghfcd1r610120241240', {
       versioned: true
     });
 
     // 👇 create VPC in which we'll launch the Instance
-    const vpc = ec2.Vpc.fromLookup(this, "VPC", {
-      isDefault: true,
+    const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 0,
+      subnetConfiguration: [
+        {name: 'public', cidrMask: 24, subnetType: ec2.SubnetType.PUBLIC},
+      ],
     });
 
-     // 👇 create Security Group for the Instance
-     const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
+    // 👇 create Security Group for the Instance
+    const webserverSG = new ec2.SecurityGroup(this, 'webserver-sg', {
       vpc,
       allowAllOutbound: true,
     });
@@ -39,34 +43,6 @@ export class HelloCdkStack extends cdk.Stack {
       ec2.Port.tcp(443),
       'allow HTTPS traffic from anywhere',
     );
-
-     // 👇 create a Role for the EC2 Instance
-     const webserverRole = new iam.Role(this, 'webserver-role', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),
-      ],
-    });
-
- // 👇 create the EC2 Instance
-    const ec2Instance = new ec2.Instance(this, 'ec2-instance', {
-      vpc,
-      vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC
-      },
-      //role: webserverRole,
-      securityGroup: webserverSG,
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.BURSTABLE2,
-        ec2.InstanceSize.MICRO,
-      ),
-      machineImage: new ec2.AmazonLinuxImage({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      }),
-      keyName: 'nareshlinux',
-    });
-
+    
   }
-
 }
-
