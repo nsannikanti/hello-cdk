14e3c74c88f7bb311adc07b9a38e340b13d3cc6f
sannikantin1008@gmail.com <sannikantin1008@gmail.com>
2024-01-30 09:50:55 +0530

    added for s3 bucket

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,61 +1,52 @@
-import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib';
-
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create a VPC
-    const vpc = new ec2.Vpc(this, 'MyVpc', {
-      maxAzs: 2, // Specify the desired number of Availability Zones
+    const s3Bucket = new s3.Bucket(this, 's3-bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      // 👇 set up lifecycle rules
+      lifecycleRules: [
+        {
+          // 👇 optionally only apply rules to objects where the prefix matches
+          // prefix: 'data/',
+          abortIncompleteMultipartUploadAfter: cdk.Duration.days(90),
+          expiration: cdk.Duration.days(365),
+          transitions: [
+            {
+              storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+              transitionAfter: cdk.Duration.days(30),
+            },
+            {
+              storageClass: s3.StorageClass.INTELLIGENT_TIERING,
+              transitionAfter: cdk.Duration.days(60),
+            },
+            {
+              storageClass: s3.StorageClass.GLACIER,
+              transitionAfter: cdk.Duration.days(90),
+            },
+            {
+              storageClass: s3.StorageClass.DEEP_ARCHIVE,
+              transitionAfter: cdk.Duration.days(180),
+            },
+          ],
+        },
+      ],
     });
 
-    // Create a Network Load Balancer
-    const nlb = new elbv2.NetworkLoadBalancer(this, 'MyNlb', {
-      vpc,
-      internetFacing: true, // Set to true if you want it to be internet-facing
-    });
-
-    // Create a listener for the NLB
-    const listener = nlb.addListener('MyListener', {
-      port: 80, // Set the desired port
-    });
-
-      // Create an Auto Scaling Group
-      const asg = new autoscaling.AutoScalingGroup(this, 'MyAsg', {
-        vpc,
-        instanceType: new ec2.InstanceType('t2.micro'), // Choose your instance type
-        machineImage: new ec2.AmazonLinuxImage(), // Use Amazon Linux as the AMI
-      });
-    // const userData = ec2.UserData.forLinux();
-    // userData.addCommands(
-    //   'sudo su',
-    //   'yum install -y httpd',
-    //   'systemctl start httpd',
-    //   'systemctl enable httpd',
-    //   'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html',
-    // );
-
-    // const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
-    //     vpc,
-    //     instanceType: ec2.InstanceType.of(
-    //       ec2.InstanceClass.BURSTABLE2,
-    //       ec2.InstanceSize.MICRO,
-    //     ),
-    //     machineImage: new ec2.AmazonLinuxImage({
-    //       generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-    //     }),
-    //     userData,
-    //     minCapacity: 2,
-    //     maxCapacity: 3,
-    //   });
-    // Add the ASG as the target to the NLB listener
-    listener.addTargets('AsgTarget', {
-      targets: [asg],
-      port: 80, // Set the port that the ASG instances are listening on
+    // 👇 add a life cycle rule after bucket creation
+    s3Bucket.addLifecycleRule({
+      prefix: 'logs/',
+      expiration: cdk.Duration.days(90),
+      transitions: [
+        {
+          storageClass: s3.StorageClass.ONE_ZONE_INFREQUENT_ACCESS,
+          transitionAfter: cdk.Duration.days(60),
+        },
+      ],
     });
   }
 }
